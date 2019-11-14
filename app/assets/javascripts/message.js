397c94282846988c990a755ad34115b1b2913681
satoshi0211wata <satoshi.2.11@gmail.com>
2019-11-14 12:25:50 +0900

    自動更新機能実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,14 +1,16 @@
 $(function(){
   function buildHTML(message){
-    var image = message.image?`<img src="${message.image}">` : " " ;
+    // var date = message.date
+    // console.log(message)
+    var image = message.image.url?`<img src="${message.image.url}">` : " " ;
       var html =
-       `<div class="message" data-message-id="${message.id}">
+       `<div class="message" data-id="${message.id}">
           <div class="upper-message">
             <div class="upper-message__user-name">
               ${message.user_name}
             </div>
             <div class="upper-message__date">
-              ${message.date}
+              ${message.created_at}
             </div>
           </div>
           <div class="lower-message">
@@ -16,7 +18,7 @@ $(function(){
               ${message.content}
             </p>
           </div>
-          ${image} 
+          ${image}
         </div>`
       return html;
   }
@@ -36,7 +38,7 @@ $(function(){
       .done(function(data){
         var html = buildHTML(data);
         $('.messages').append(html);
-        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');  
         $('form')[0].reset();
       })
       .fail(function(){
@@ -44,4 +46,32 @@ $(function(){
       });
       return false;
     });
-});
+    var reloadMessages = function() {
+      if(document.URL.match("/messages")){
+      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+      last_message_id = $('.message:last').data('id');
+      var url = location.href.replace("messages","api/messages");
+      $.ajax({
+        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+        url: url,
+        //ルーティングで設定した通りhttpメソッドをgetに指定
+        type: 'get',
+        dataType: 'json',
+        //dataオプションでリクエストに値を含める
+        data: {id: last_message_id}
+      })
+      .done(function(messages){
+        messages.forEach(function(message){
+        var insertHTML = buildHTML(message);
+        $('.messages').append(insertHTML);
+      })
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+        
+      })
+      .fail(function() {
+        alert('error');
+      });
+      }
+    };     
+    setInterval(reloadMessages, 7000);
+})
