e8d7ac14f52b47649927b1840c3524618044b95d
satoshi0211wata <satoshi.2.11@gmail.com>
2019-11-14 12:54:35 +0900

    コメントアウト削除

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,5 @@
 $(function(){
   function buildHTML(message){
-    // var date = message.date
-    // console.log(message)
     var image = message.image.url?`<img src="${message.image.url}">` : " " ;
       var html =
        `<div class="message" data-id="${message.id}">
@@ -48,16 +46,12 @@ $(function(){
     });
     var reloadMessages = function() {
       if(document.URL.match("/messages")){
-      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
       last_message_id = $('.message:last').data('id');
       var url = location.href.replace("messages","api/messages");
       $.ajax({
-        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
         url: url,
-        //ルーティングで設定した通りhttpメソッドをgetに指定
         type: 'get',
         dataType: 'json',
-        //dataオプションでリクエストに値を含める
         data: {id: last_message_id}
       })
       .done(function(messages){
